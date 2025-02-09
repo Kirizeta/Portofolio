6e33a36f4c64fe6b825280f95730a7b58c0db1d0
Arif Musadi <89764450+Kirizeta@users.noreply.github.com>
2025-02-10 00:12:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
+  const icon = document.querySelector(".Porto-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
