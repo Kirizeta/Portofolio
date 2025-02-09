b2f4cca9a7f1c6cfc3cbf1f6233ac4c1fcd95646
Arif Musadi <89764450+Kirizeta@users.noreply.github.com>
2025-02-10 00:31:34 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".Porto-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
-
