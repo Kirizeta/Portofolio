373f24baec75304e9521daf2a7a30e078ef9b1e5
Arif Musadi <89764450+Kirizeta@users.noreply.github.com>
2025-08-16 21:34:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".Porto-icon");
+  const expanded = icon.getAttribute("aria-expanded") === "true";
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  icon.setAttribute("aria-expanded", (!expanded).toString());
 }
