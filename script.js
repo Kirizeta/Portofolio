62dfd68128e52aafe0975568af981a66dddaf25f
Arif Musadi <89764450+Kirizeta@users.noreply.github.com>
2025-08-16 21:45:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function toggleMenu() {
   icon.classList.toggle("open");
   icon.setAttribute("aria-expanded", (!expanded).toString());
 }
+
+  function openGmailCompose() {
+    const to = "muslimarif2@gmail.com";
+    const subject = encodeURIComponent("Halo Arif");
+    const body = encodeURIComponent("Halo, saya ingin bertanya...");
+    const url = `https://mail.google.com/mail/?view=cm&fs=1&to=${to}&su=${subject}&body=${body}`;
+    // buka tab baru
+    window.open(url, "_blank");
+  }
